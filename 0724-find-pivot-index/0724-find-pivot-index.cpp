a678b7176dbc5e8add1c8eaa85c39bffbd9e89c1
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-07-04 19:01:51 +0530

    Time: 0 ms (100%), Space: 35.5 MB (96.83%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -8,12 +8,18 @@ public:
         }
         int left=0;
         int right;
+        /*
         for (int i=0;i<1;i++){
             left=0;
             right=sum-nums[i]-left;
             if (left==right){
                 return 0;
             }
+        }
+        */
+        right=sum-nums[0];
+        if (left==right){
+            return 0;
         }
         for (int i=1;i<n;i++){
             left=left+nums[i-1];
