3afc745c4584941dec402300447582d34f3ac8c1
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-07-08 21:30:31 +0530

    Time: 0 ms (100%), Space: 22.9 MB (84.8%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -11,25 +11,25 @@ public:
             int end1=firstList[i][1];
             int start2=secondList[j][0];
             int end2=secondList[j][1];
-            if (start1<=start2){
-                if (end1>=start2){
-                    int s=max(start1,start2);
-                    int e=min(end1,end2);
+            if (start1<=start2){      //  First interval starts earlier.
+                if (end1>=start2){       // Check if they overlap.
+                    int s=max(start1,start2);  // intersection formula
+                    int e=min(end1,end2);      // intersection formula
                     res.push_back({s,e});
                 }
             }
-            else {
-                if (end2>=start1){
-                    int s=max(start1,start2);
-                    int e=min(end1,end2);
+            else {     //  Second interval starts earlier..
+                if (end2>=start1){    // check if they overlap..
+                    int s=max(start1,start2);  // same intersection formula 
+                    int e=min(end1,end2);   // same intersection formula or condition
                     res.push_back({s,e});
                 }
             }
             if (end1>end2){
-                j++;
+                j++;        // // Second interval ends first, so move j.
             }
             else {
-                i++;
+                i++;    //First interval ends first (or both end together), so move i.
             }
         }
         return res;
