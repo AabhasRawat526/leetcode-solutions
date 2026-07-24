d508ef96223137acf13f12fcd807b56ee49fd2b7
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-07-24 18:48:49 +0530

    Time: 7 ms (64.08%), Space: 15.3 MB (28.65%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int n=s.size();
+        char op='+';
+        int number=0;
+        int res=0;
+        stack <int> st;
+        for (int i=0;i<n;i++){
+            if(isdigit(s[i])){
+                number=(number*10)+(s[i]-'0');
+            }
+            if ((!isdigit(s[i]) && s[i] != ' ') || i==n-1){
+                 if (op=='+'){
+                    st.push(number);
+                }
+                else if (op=='-'){
+                    st.push(-number);
+                }
+                else if (op=='*'){
+                    int top=st.top();
+                    st.pop();
+                    st.push(top*number);
+                }
+                else if (op=='/'){
+                    int top=st.top();
+                    st.pop();
+                    st.push(top/number);
+                }
+                op=s[i];
+                number=0;
+            }
+        }
+        int ans=0;
+        while(!st.empty()){
+            ans=ans+st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
