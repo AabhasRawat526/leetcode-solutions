673f8e94b01153f110c1b98fed2197de09c14fda
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-07-25 01:09:27 +0530

    Time: 4 ms (29.31%), Space: 12.3 MB (6.28%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minLength(string s) {
+        int n=s.size();
+        stack<int> st;
+        for (int i=0;i<n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+                continue;
+            }
+            if (st.top()=='A' && s[i]=='B'){
+                st.pop();
+                continue;
+            }
+            if (st.top()=='C' && s[i]=='D'){
+                st.pop();
+                continue;
+            }
+            st.push(s[i]);
+        }
+        return st.size();
+    }
+};
