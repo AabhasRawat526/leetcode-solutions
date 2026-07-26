fd7df8e5267b41e52a6f5e30936c0097e1b373f2
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-07-26 19:03:08 +0530

    Time: 6 ms (53.59%), Space: 32.5 MB (1.76%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,13 +1,48 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        int n=nums.size();
         // idea is we can get max product when we multiply first largest*second largest*third largest
-        // we can also get max product when we multiply first smallest * second smallest*third smallest.
-        sort(nums.begin(),nums.end());
+        // we can also get max product when we multiply first smallest * second smallest*first largest.
+        /*
+        tc is 0(n logn)
+        int n=nums.size();
         int product=(nums[n-1]*nums[n-2]*nums[n-3]);
         int product1=(nums[0]*nums[1]*nums[n-1]);
         int maximumproduct=max(product,product1);
         return maximumproduct;
+
+        */
+
+        int n=nums.size();
+
+        vector <int> res=nums;
+
+        int maximum1=*max_element(res.begin(),res.end());
+        auto id=max_element(res.begin(),res.end());
+        res.erase(id);
+
+        int maximum2=*max_element(res.begin(),res.end());
+        auto it=max_element(res.begin(),res.end());
+        res.erase(it);
+
+        int maximum3=*max_element(res.begin(),res.end());
+        auto im=max_element(res.begin(),res.end());
+        res.erase(im);
+
+        int smallest1=*min_element(nums.begin(),nums.end());
+        auto is=min_element(nums.begin(),nums.end());
+        nums.erase(is);
+
+        int smallest2=*min_element(nums.begin(),nums.end());
+        auto in=min_element(nums.begin(),nums.end());
+        nums.erase(in);
+
+        int product1=maximum1*maximum2*maximum3;
+
+        int product2=maximum1*smallest1*smallest2;
+
+        int product=max(product1,product2);
+
+        return product;
     }
 };
