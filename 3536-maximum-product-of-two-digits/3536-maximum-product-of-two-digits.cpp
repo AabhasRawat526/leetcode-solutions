78fc030b1413a5ccef638d8b1a1aa4d90d52ac55
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-07-26 17:41:49 +0530

    Time: 3 ms (14.82%), Space: 9.2 MB (13.65%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -11,8 +11,8 @@ public:
         s.erase(it);
         int number1=0;
         int number2=0;
-        number1=(number1*10)+maximum1-'0';
-        number2=(number2*10)+maximum2-'0';
+        number1=(number1*10)+maximum1-'0';     // ascii value will get multplied like 0*10+53-48 = 3 53 is ascii value of '3' and 48 is ascii value of '0' 
+        number2=(number2*10)+maximum2-'0';   // same goes for this 
         return number1*number2;
     }
 };
