ddc5b5b594adc00b713f32a5932fba9d5bffbe75
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-07-27 19:05:38 +0530

    Time: 0 ms (100%), Space: 10.3 MB (90.17%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+
+        /*
         int n=s.size();
         stack <int> st;
         int length=0;
@@ -17,6 +19,46 @@ public:
                 else {
                     length=max(length,i-st.top());
                 }
+            }
+        }
+        return length;
+
+        */
+
+        int left=0;
+        int right=0;
+        int length=0;
+        int n=s.size();
+        for (int i=0;i<n;i++){
+            if (s[i]=='('){
+                left++;
+            }
+            else {
+                right++;
+            }
+            if (left==right){
+                length=max(length,left*2);
+            }
+            else if (right>left){
+                left=0;
+                right=0;
+            }
+        }
+        left=0;
+        right=0;
+        for (int i=n-1;i>=0;i--){
+            if(s[i]=='('){
+                left++;
+            }
+            else {
+                right++;
+            }
+            if (left==right){
+                length=max(length,left*2);
+            }
+            else if(left>right){
+                left=0;
+                right=0;
             }
         }
         return length;
