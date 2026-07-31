7e7d59b57a561f7082f995c03bdfafe50e59e7c5
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-07-31 11:57:56 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.24%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        /*
-        piegon hole problem as we have fixed 8 numbers like we have fixed holes in piegon 
+        
+        //piegon hole problem as we have fixed 8 numbers like we have fixed holes in piegon 
         int n=word.size();
         int res=0;
         for (int i=0;i<n;i++){
             res=res+(i/8+1);
         }
         return res;
-        */
-
+        
+        /*
         int n=word.size();
         for (int i=0;i<n;i++){
             if (n<=8){
@@ -27,5 +27,6 @@ public:
             }
         }
         return -1;
+        */
     }
 };
