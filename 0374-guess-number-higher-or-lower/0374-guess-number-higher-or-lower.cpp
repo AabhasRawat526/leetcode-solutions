d17060cbc3acb5ce951ebd2b7aafadcfad949205
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-08-01 13:14:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (75.5%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -4,7 +4,7 @@
  * @return 	     -1 if num is higher than the picked number
  *			      1 if num is lower than the picked number
  *               otherwise return 0
- * int guess(int num);
+ * int guess(int num); 
  */
 
 class Solution {
@@ -14,12 +14,12 @@ public:
         int right=n;
         while(left<=right){
             int mid=left+(right-left)/2;    // to prevent the overflow we used this formula 
-            int guess_no=guess(mid);
+            int guess_no=guess(mid);   // internal it will compare the our mid value to the number which it picks gives 0 when we got gives -1 when it is bigger and gives 1 when it is smaller..
 
             if (guess_no==0){   // number mil gya 
                 return mid;
             }
-            else if (guess_no==-1){   // number bahut bada hai 
+            else if (guess_no==-1){   // number bahut bada hai  given 
                 right=mid-1;
             }
             else {
