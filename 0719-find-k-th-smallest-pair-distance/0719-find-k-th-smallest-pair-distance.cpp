701cc3b060147229aa7297f761cd75754cc7c04f
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-08-10 00:46:50 +0530

    Time: 3 ms (56.39%), Space: 13.9 MB (96.18%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+
+
+int smallestpairdistance(vector<int>& nums,int distance){
+    int i=0;
+    int j=1;
+    int paircount=0;
+    int n=nums.size();
+    while(j<n){
+        while(abs(nums[j]-nums[i])>distance){
+            i++;
+        }
+        paircount=paircount+(j-i);
+        j++;
+    }
+    return paircount;
+}
+
+
     int smallestDistancePair(vector<int>& nums, int k) {
         /*
         Brute Force
@@ -23,6 +41,8 @@ public:
         return -1;
         */
 
+        /*
+        another brute force but this time with the help of partial sorting
         int n=nums.size();
         vector <int> res;
         for (int i=0;i<n;i++){
@@ -31,7 +51,27 @@ public:
                 res.push_back(difference);
             }
         }
-        nth_element(res.begin(),res.begin()+(k-1),res.end());
+        nth_element(res.begin(),res.begin()+(k-1),res.end()); // this means put the smallest element in k-1 place..
         return res[k-1];
+
+        */
+
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int left=0;
+        int right=nums[n-1]-nums[0];
+        int res=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int possible=smallestpairdistance(nums,mid);
+            if (possible<k){
+                left=mid+1;
+            }
+            else {
+                res=mid;
+                right=mid-1;
+            }
+        }
+        return res;
     }
 };
