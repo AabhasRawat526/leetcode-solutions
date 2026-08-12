8fb690281ceed1fc6d354996f3a38c1d4ffb0bd8
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-08-12 13:19:26 +0530

    Time: 0 ms (100%), Space: 23.1 MB (6.55%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -17,10 +17,14 @@ public:
         for (int i=0;i<m;i++){
             sum=sum+res[i];
         }
+        vector <int> ans(n);
+        for (int i=0;i<n;i++){
+            ans[i]=nums[i];
+        }
+        sort(ans.begin(),ans.end());
         for(int i=0;i<n;i++){
-            if (sum==nums[i]){
+            if (sum==ans[i]){
                 sum++;
-                i=0;
             }
         }
         return sum;
