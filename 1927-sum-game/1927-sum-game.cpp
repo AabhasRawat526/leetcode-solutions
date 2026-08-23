baf32ec43ec89dc699788c4f49b0c7725c16da85
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-08-24 02:02:16 +0530

    Time: 7 ms (21.62%), Space: 13.9 MB (67.7%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n=num.size();
+        int leftquestionmark=0;
+        int rightquestionmark=0;
+        int leftsum=0;
+        int rightsum=0;
+        for (int i=0;i<n;i++){
+            if (num[i]=='?'){
+                if (i<n/2){
+                    leftquestionmark++;
+                }
+                else {
+                    rightquestionmark++;
+                }
+            }
+            else {
+                if (i<n/2){
+                    leftsum=leftsum+(num[i]-'0');
+                }
+                else {
+                    rightsum=rightsum+(num[i]-'0');
+                }
+            }
+        }
+        int totalquestioncount=leftquestionmark+rightquestionmark;
+        if ((totalquestioncount%2)!=0){
+            return true;
+        }
+        int LEFTSUM=2*leftsum + 9*leftquestionmark;
+
+        int RIGHTSUM=2*rightsum + 9*rightquestionmark;
+
+        if (LEFTSUM==RIGHTSUM){
+            return false;
+        }
+        return true;
+    }
+};
