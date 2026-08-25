73c86140c686d52c44127d40873b99e9517e75b3
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-08-25 21:51:28 +0530

    Time: 47 ms (54.59%), Space: 68.5 MB (64.03%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>>res;
+        if (points.size()==0){
+            return res;
+        }
+        priority_queue<pair<double,int>,vector<pair<double,int>>>pq; // max heap me nahi banayenge bcmp wala and all structure...
+        int n=points.size();
+        for (int i=0;i<n;i++){
+            int x=points[i][0];
+            int y=points[i][1];
+            double distance=sqrt(x*x+y*y);
+            pair<double,int>current={distance,i};
+            if (pq.size()<k){
+                pq.push(current);
+                continue;
+            }
+            if (pq.top()<=current){  // max heap me <= and min heap me >=
+                continue;
+            }
+            else {
+                pq.pop();
+                pq.push(current);
+            }
+        }
+        while(!pq.empty()){
+            res.push_back(points[pq.top().second]);
+            pq.pop();
+        }
+        return res;
+    }
+};
