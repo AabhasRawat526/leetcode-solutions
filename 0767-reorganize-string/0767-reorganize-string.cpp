092fd1af46094712e183aa82fad1322249b60f31
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-08-27 00:03:16 +0530

    Time: 0 ms (100%), Space: 8.4 MB (79.51%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n=s.size();
+        string res="";
+        unordered_map<char,int> f;
+        for (int i=0;i<n;i++){
+            f[s[i]]++;
+        }
+        priority_queue<pair<int,char>,vector<pair<int,char>>> pq;
+        for(auto id:f){
+            pq.push({id.second,id.first});
+        }
+        int seat=0;
+        while(!pq.empty()){
+            pair<int,char> current={pq.top().first,pq.top().second};
+            if(seat==0 || res[seat-1]!=current.second){
+                res.push_back(current.second);
+                pq.pop();
+                current.first--;
+                seat++;
+                if (current.first >0){
+                    pq.push(current);
+                }
+            }
+            else {
+                pair<int,char> current={pq.top().first,pq.top().second};
+                pq.pop();
+                if (pq.empty()){
+                    return "";
+                }
+                pair<int,char> current2={pq.top().first,pq.top().second};
+                pq.pop();
+                res.push_back(current2.second);
+                seat++;
+                current2.first--;
+                if (current2.first>0){
+                    pq.push(current2);
+                }
+                pq.push(current);
+            }
+        }
+        if (res.size()!=s.size()){
+            return "";
+        }
+        return res;
+    }
+};
