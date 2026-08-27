53e860e468a5af72294b3e6d0b13ce9f8734fa48
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-08-27 13:48:11 +0530

    Time: 0 ms (100%), Space: 8.8 MB (45.52%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -6,7 +6,8 @@ public:
         int j=0;
         int one=0;
         string result="";
-        while(j<n){
+        // tc is 0(n^2)  because of while loop and the substr...
+        while(j<n){  // tc is 0(n)
             if(s[j]=='1'){
                 one++;
             }
@@ -17,8 +18,8 @@ public:
                 i++;
             }
             if (one==k){
-                string temp=s.substr(i,j-i+1);
-                if (result.empty() || result.length()>j-i+1 || result.length()== temp.length() && temp<result){
+                string temp=s.substr(i,j-i+1); // because of substr tc is 0(n)
+                if (result.empty() || result.length()>temp.length() || result.length()== temp.length() && temp<result){
                     result=temp;
                 }
             }
