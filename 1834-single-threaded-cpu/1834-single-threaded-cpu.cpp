8e6f5c9e65d0d36d6bf984ce66dd96c5453ba93f
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-05 02:16:39 +0530

    Time: 194 ms (72.49%), Space: 304 MB (65.6%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         int n=tasks.size();
-        vector<array<int,3>> sortedArray;
+        vector<array<int,3>>sortedArray;
         for (int i=0;i<n;i++){
-            int start_time=tasks[i][0];
-            int process_time=tasks[i][1];
-            sortedArray.push_back({start_time,process_time,i});
+            int starttime=tasks[i][0];
+            int processtime=tasks[i][1];
+            sortedArray.push_back({starttime,processtime,i});
         }
         sort(sortedArray.begin(),sortedArray.end());
-        long long int currenttime=0;
-        vector<int> res;
+        vector<int>res;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        long long int currenttime=0;
         int idx=0;
         while(idx<n || !pq.empty()){
             if (pq.empty() && currenttime<sortedArray[idx][0]){
                 currenttime=sortedArray[idx][0];
             }
-            while(idx<n && sortedArray[idx][0]<=currenttime){
+            while(idx<n && currenttime>=sortedArray[idx][0]){
                 pq.push({sortedArray[idx][1],sortedArray[idx][2]});
                 idx++;
             }
             pair<int,int>curr=pq.top();
             pq.pop();
-            res.push_back(curr.second);
             currenttime=currenttime+curr.first;
+            res.push_back(curr.second);
         }
         return res;
     }
