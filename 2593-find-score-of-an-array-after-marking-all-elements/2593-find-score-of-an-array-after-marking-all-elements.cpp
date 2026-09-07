9b251b2f0e036f34a538bca8a647047b1d7e32cc
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-08 02:49:05 +0530

    Time: 116 ms (66.83%), Space: 108.7 MB (39.53%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+typedef pair<int,int> p;
+
+    long long findScore(vector<int>& nums) {
+        int n=nums.size();
+        priority_queue<p,vector<p>,greater<p>> pq;  // min heap of pairs
+        vector<int> temp(n,false);
+        for (int i=0;i<n;i++){
+            pq.push({nums[i],i});
+        }
+        long long int score=0;
+        while(!pq.empty()){
+            p result=pq.top();
+            int element=result.first;
+            int idx=result.second;
+            pq.pop();
+            if (temp[idx]==false){
+                temp[idx]=true;
+                score=score+element;
+                if (idx-1>=0 && temp[idx-1]==false){
+                    temp[idx-1]=true;
+                }
+                if (idx+1<n && temp[idx+1]==false){
+                    temp[idx+1]=true;
+                }
+            }
+        }
+        return score;
+    }
+};
