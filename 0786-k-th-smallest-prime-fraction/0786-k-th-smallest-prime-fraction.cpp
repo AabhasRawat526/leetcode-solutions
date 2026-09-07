92a6cb5f4fcf63575b6a8a4ee1d342c4ae5a0c4f
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-07 12:22:11 +0530

    Time: 796 ms (19.72%), Space: 179.1 MB (13.79%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,22 +1,51 @@
 class Solution {
 public:
-    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        int n=arr.size();
-        priority_queue <vector<double>> pq; // by default it is max heap
-        for (int i=0;i<n-1;i++){
-            for (int j=i+1;j<n;j++){
-                double fraction=(double) arr[i]/arr[j];
-                pq.push(vector<double>{fraction,(double) arr[i],(double) arr[j]});
 
-                if(pq.size()>k){
-                    pq.pop();
-                }
-            }
+typedef vector<double> V;
+
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        // int n=arr.size();
+        // priority_queue <vector<double>> pq; // by default it is max heap
+        // for (int i=0;i<n-1;i++){
+        //     for (int j=i+1;j<n;j++){
+        //         double fraction=(double) arr[i]/arr[j];
+        //         pq.push(vector<double>{fraction,(double) arr[i],(double) arr[j]});
+
+        //         if(pq.size()>k){
+        //             pq.pop();
+        //         }
+        //     }
+        // }
+        // auto vec=pq.top();
+        // vector<int>res(2);  // size of the vector is 2....
+        // res[0]=vec[1];
+        // res[1]=vec[2];
+        // return res;
+
+        int n=arr.size();
+
+        priority_queue<V,vector<V>,greater<V>> pq;
+        for(int i=0;i<n-1;i++){
+            double fraction=1.0*arr[i]/arr[n-1];
+            pq.push({fraction,(double) i ,(double) n-1});
         }
-        auto vec=pq.top();
-        vector<int>res(2);  // size of the vector is 2....
-        res[0]=vec[1];
-        res[1]=vec[2];
-        return res;
+
+        int smallest=1;
+
+        while(smallest<k){
+            V vec=pq.top();
+            pq.pop();
+            int i=vec[1];   // we are finding the index here
+            int j=vec[2]-1;
+
+            pq.push({1.0*arr[i]/arr[j],(double) i ,(double) j});
+
+            smallest++;
+        }
+
+        V vec=pq.top();
+        int i=vec[1];
+        int j=vec[2];
+        return {arr[i],arr[j]};
     }
 };
