e73eb58280e7ddbca1e0a79f61c4ad556e34b3b2
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-08 01:10:17 +0530

    Time: 63 ms (88.43%), Space: 11.1 MB (96.27%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,23 +1,54 @@
 class Solution {
 public:
+
+typedef pair<int,int>p;
+
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        // brute force;
+        // // brute force;
+        // int M=1e9+7;
+        // int m=nums.size();
+        // vector<int> temp;
+        // for (int i=0;i<m;i++){
+        //     int sum=0;
+        //     for (int j=i;j<m;j++){
+        //         sum=sum+nums[j];
+        //         temp.push_back(sum);
+        //     }
+        // }
+
+        // sort(temp.begin(),temp.end());
+        // long long int res=0;   // here left and right is based on the 1 based indexing not on the 0 based indexing
+        // for (int i=left-1;i<=right-1;i++){
+        //     res=(res+temp[i])%M;
+        // }
+        // return res;
+
+        // optimal solution ko by using heap 
+
         int M=1e9+7;
         int m=nums.size();
-        vector<int> temp;
+        priority_queue<p,vector<p>,greater<p>> pq;
         for (int i=0;i<m;i++){
-            int sum=0;
-            for (int j=i;j<m;j++){
-                sum=sum+nums[j];
-                temp.push_back(sum);
+            pq.push({nums[i],i});
+        }
+        int result=0;
+        for (int count=1;count<=right;count++){
+            auto data=pq.top();
+            pq.pop();
+
+            int sum=data.first;
+            int idx=data.second;
+
+            if (count>=left){
+                result=(result+sum)%M;
+            }
+
+            int new_idx=idx+1;
+            if (new_idx<n){
+                int new_sum=sum+nums[new_idx];
+                pq.push({new_sum,new_idx});
             }
         }
-
-        sort(temp.begin(),temp.end());
-        long long int res=0;   // here left and right is based on the 1 based indexing not on the 0 based indexing
-        for (int i=left-1;i<=right-1;i++){
-            res=(res+temp[i])%M;
-        }
-        return res;
+        return result;
     }
 };
