e90f1b13fa6990969142f0a46a0074d8ddf2a0aa
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-09 01:27:35 +0530

    Time: 23 ms (8.9%), Space: 202.5 MB (71.26%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -6,13 +6,13 @@ public:
         int minvalue=INT_MAX;
         for(int i=n-1;i>=0;i--){
             minvalue=min(minvalue,nums[i]);
-            minelementindex[i]=minvalue;
+            minelementindex[i]=minvalue;  // arr[3]=2 ====>> [1,_,3] yaha pe me array bana raha hu 
         }
         int maxvalue=INT_MIN;
         for (int i=0;i<n;i++){
             maxvalue=max(maxvalue,nums[i]);
-            minvalue=minelementindex[i];
-            if ((maxvalue-minvalue)<=k){
+            int value=minelementindex[i];  // minvalue=arr[0] minvalue me kya aajayega that is 1 and yaha pe me array ki value ko kisi aur variable me daaal raha hu .....
+            if ((maxvalue-value)<=k){  // 
                 return i;
             }
         }
