d7d74e4776104391525d3aef05105f725e306ca7
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-08 16:00:52 +0530

    Time: 258 ms (10.72%), Space: 226.2 MB (14.29%) - LeetHub

--- 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.cpp
+++ 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.cpp
@@ -3,29 +3,27 @@ public:
     vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
         int n=nums1.size();
         vector<vector<int>> res;
-        for (int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             res.push_back({nums1[i],i,nums2[i]});
         }
-
         sort(res.begin(),res.end());
-        long long int result=0;
-        priority_queue<int,vector<int>,greater<>>pq;
-        vector<long long>answer(n);
+        priority_queue<int,vector<int>,greater<int>>pq;
+        long long int sum=0;
+        vector<long long> ans(n);
         for (int i=0;i<n;i++){
             if (i>0 and res[i-1][0]==res[i][0]){
-                answer[res[i][1]]=answer[res[i-1][1]];
+                ans[res[i][1]]=ans[res[i-1][1]];
             }
             else {
-                answer[res[i][1]]=result;
+                ans[res[i][1]]=sum;
             }
             pq.push(res[i][2]);
-            result=result+res[i][2];
+            sum=sum+res[i][2];
             if (pq.size()>k){
-                result=result-pq.top();
+                sum=sum-pq.top();
                 pq.pop();
             }
-
         }
-        return answer;
+        return ans;
     }
 };
