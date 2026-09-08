18fa2acb18db47f074bbee1eeac9aab7b3b1e171
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-08 16:09:08 +0530

    Time: 259 ms (10.49%), Space: 226.1 MB (22.55%) - LeetHub

--- 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.cpp
+++ 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.cpp
@@ -3,14 +3,14 @@ public:
     vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
         int n=nums1.size();
         vector<vector<int>> res;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){   // tc is 0(n)
             res.push_back({nums1[i],i,nums2[i]});
         }
-        sort(res.begin(),res.end());
+        sort(res.begin(),res.end());  // tc is 0(n logn)
         priority_queue<int,vector<int>,greater<int>>pq;
         long long int sum=0;
         vector<long long> ans(n);
-        for (int i=0;i<n;i++){
+        for (int i=0;i<n;i++){  // tc is 0(n) and because of push and pop there will be tc 0(n logk)
             if (i>0 and res[i-1][0]==res[i][0]){
                 ans[res[i][1]]=ans[res[i-1][1]];
             }
