97cc6069b5c92e7e493408c53e05e1810d000dba
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-14 12:21:15 +0530

    Time: 2 ms (45.33%), Space: 14.1 MB (46.39%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+void solve(int currentnumber,int n,vector<int> &res){
+    if (currentnumber>n){
+        return ;
+    }
+    res.push_back(currentnumber);
+    for (int append=0;append<=9;append++){
+        int newnumber=(currentnumber*10)+append;
+        if (newnumber>n){
+            return;
+        }
+
+        solve(newnumber,n,res);
+    }
+}
+
+    vector<int> lexicalOrder(int n) {
+       vector<int>res;
+       for (int startnum=1;startnum<=9;startnum++){
+            solve(startnum,n,res);
+       }
+       return res;
+    }
+};
