b5d3ee889a2be2c176bd2064b45658a7535f8104
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-15 21:39:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (64.13%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+
+
+int findwinnerindex(int n,int k){
+    if (n==1){
+        return 0; // means the element will be present in the zero index
+    }
+
+    int idx=findwinnerindex(n-1,k);
+    idx=(idx+k)%n;
+    return idx;
+}
+
     int findTheWinner(int n, int k) {
         /*
         vector<int>arr;
@@ -14,18 +26,23 @@ public:
         }
         return arr[0];
         */
+        /*
         queue<int> que;
         for (int i=1;i<=n;i++){
             que.push(i);
         }
-        while(que.size()>1){
-            for(int i=1;i<=k-1;i++){
-                que.push(que.front());
-                que.pop();
+        while(que.size()>1){  // 0(n)
+            for(int i=1;i<=k-1;i++){ // 0(k) so total tc is 0(n*k)
+                que.push(que.front()); // pushing k-1 element to the last 
+                que.pop(); // popping k-1 element 
             }
-            que.pop();
+            que.pop(); // popping kth element ... that is what we needed...
         }
         return que.front();
+        */
+
+        int resultantidx=findwinnerindex(n,k);
+        return resultantidx+1;
     }
 };
 
