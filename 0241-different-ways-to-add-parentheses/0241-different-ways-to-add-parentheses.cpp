93c07ebeea183feea3f35d70c3052cae9fbfb834
Aabhas Rawat <aabhasrawat1703@gmail.com>
2026-09-18 13:39:31 +0530

    Time: 4 ms (30.34%), Space: 14.1 MB (43.13%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+vector<int> solve(string expression){
+    vector<int>res;
+    int n=expression.size();
+    for (int i=0;i<n;i++){
+        if (expression[i]=='+' || expression[i]=='-' || expression[i]=='*'){
+            vector<int> left_sum=solve(expression.substr(0,i));
+            vector<int> right_sum=solve(expression.substr(i+1));
+            for (int x:left_sum){
+                for (int y:right_sum){
+                    if (expression[i]=='+'){
+                        res.push_back(x+y);
+                    }
+                    else if (expression[i]=='-'){
+                        res.push_back(x-y);
+                    }
+                    else {
+                        res.push_back(x*y);
+                    }
+                }
+            }
+        }
+    }
+    if (res.empty()){
+        res.push_back(stoi(expression));
+    }
+    return res;
+}
+
+    vector<int> diffWaysToCompute(string expression) {
+        return solve(expression);
+    }
+};
